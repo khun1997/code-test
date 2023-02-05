31835f53e47f3760b4f7e0638f799d3fdff6b2e9
Khun Ye Aung <kkhun531@gmail.com>
2023-02-05 12:46:47 +0630

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 // import './App.css';
 
@@ -56,140 +56,289 @@ import styled from 'styled-components';
 //PASS PROPS
 
 // const Input = styled.input`
-// background: yellow;
-// color: ${ props => props.inputColor || 'black'};
-// border: 3px solid black;
-// margin: 1em;
-// padding: 0.5em;
-// border-radius:10px;
+// // background: yellow;
+// // color: ${ props => props.inputColor || 'black'};
+// // border: 3px solid black;
+// // margin: 1em;
+// // padding: 0.5em;
+// // border-radius:10px;
 
+// // `;
+
+// // function App(){
+// //   return(
+// //     <div>
+// //       <Input defaultValue='this is one'/>
+// //       <br/>
+// //       <Input defaultValue='this is two' inputColor='red'/>
+
+
+// //     </div>
+// //   );
+// // }
+// // const StyledWrapper = styled.div`
+
+
+
+// // function App(){
+// //   return(
+// //     return(
+// //       <StyledWrapper></StyledWrapper>
+// //     )
+// //   );
+// // }
+
+
+// //PSEUDO TEST
+// // const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
+// //   color: blue;
+// //   margin: 1em;
+// //   font-size:3em;
+// //   font-weight:bold;
+// //   &:hover {
+// //     color: black;
+// //     padding:1em;
+// //     background:red; // <Thing> when hovered
+// //   }
+
+// //   & ~ & {
+// //     background: yellow; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
+// //   }
+// //   & + & {
+// //     background: lime; // <Thing> next to <Thing>
+// //   }
+// //   &.something {
+// //     background: orange; 
+// //     /* color:white;// <Thing> tagged with an additional CSS class ".something" */
+// //   }
+// //   .something-else & {
+// //     border: 4px solid; 
+// //     // <Thing> inside another element labeled ".something-else"
+// //   }
+// // `
+// // function App(){
+// //   return(
+// //     <React.Fragment>
+// //     <Thing>One</Thing>
+// //     <Thing>Two</Thing>
+// //     <Thing className="something">Three</Thing>
+// //     <div>Four</div>
+// //     <Thing>Five</Thing>
+// //     <div className="something-else">
+// //       <Thing>Six</Thing>
+// //     </div>
+// //   </React.Fragment>
+// //   )
+
+// // }
+
+// const Input = styled.input.attrs({ type: "checkbox" })``;
+
+// const Label = styled.label`
+//   align-items: center;
+//   display: flex;
+//   gap: 8px;
+//   margin-bottom: 8px;
+// `
+
+// const LabelText = styled.span`
+// //   ${(props) => {
+// //     switch (props.$mode) {
+// //       case "dark":
+// //         return `
+// //           background-color: black;
+// //           color: white;
+// //           ${Input}:checked + && {
+// //             color: green;
+// //           }
+// //         `;
+// //       default:
+// //         return `
+// //           background-color: yellow;
+// //           color: black;
+// //           ${Input}:checked + && {
+// //             color: red;
+// //           }
+// //         `;
+// //     }
+// //   }}
+// // `;
+// // function App(){
+// //   return(
+// //     <React.Fragment>
+// //     <Label>
+// //       <Input defaultChecked />
+// //       <LabelText>one</LabelText>
+// //     </Label>
+
+// //     <Label>
+// //       <Input />
+// //       <LabelText $mode="dark">two</LabelText>
+// //     </Label>
+
+// //     <Label>
+// //       <Input defaultChecked />
+// //       <LabelText>three</LabelText>
+// //     </Label>
+
+// //     <Label>
+// //       <Input defaultChecked />
+// //       <LabelText $mode="dark">four</LabelText>
+// //     </Label>
+// //   </React.Fragment>
+// //   )
+// // }
+
+
+// //test
+// const Input = styled.input.attrs({ type: "checkbox" })``;
+
+// const Label = styled.label`
+//   align-items: center;
+//   display: flex;
+//   gap: 8px;
+//   margin-bottom: 8px;
+// `
+
+// const LabelText = styled.span`
+//   ${(props) => {
+//     switch (props.$mode) {
+//       case "dark":
+//         return `
+//           background-color: black;
+//           color: white;
+//           ${Input}:checked + && {
+//             color: blue;
+//           }
+//         `;
+//       default:
+//         return `
+//           background-color: yellow;
+//           color: black;
+//           ${Input}:checked + && {
+//             color: red;
+//           }
+//         `;
+//     }
+//   }}
 // `;
 
-// function App(){
-//   return(
-//     <div>
-//       <Input defaultValue='this is one'/>
-//       <br/>
-//       <Input defaultValue='this is two' inputColor='red'/>
 
-
-//     </div>
-//   );
-// }
-// const StyledWrapper = styled.div`
-
-
-
-// function App(){
-//   return(
-//     return(
-//       <StyledWrapper></StyledWrapper>
-//     )
-//   );
-// }
-
-
-//PSEUDO TEST
-// const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
-//   color: blue;
-//   margin: 1em;
-//   font-size:3em;
-//   font-weight:bold;
-//   &:hover {
-//     color: black;
-//     padding:1em;
-//     background:red; // <Thing> when hovered
-//   }
-
-//   & ~ & {
-//     background: yellow; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
-//   }
-//   & + & {
-//     background: lime; // <Thing> next to <Thing>
-//   }
-//   &.something {
-//     background: orange; 
-//     /* color:white;// <Thing> tagged with an additional CSS class ".something" */
-//   }
-//   .something-else & {
-//     border: 4px solid; 
-//     // <Thing> inside another element labeled ".something-else"
-//   }
-// `
 // function App(){
 //   return(
 //     <React.Fragment>
-//     <Thing>One</Thing>
-//     <Thing>Two</Thing>
-//     <Thing className="something">Three</Thing>
-//     <div>Four</div>
-//     <Thing>Five</Thing>
-//     <div className="something-else">
-//       <Thing>Six</Thing>
-//     </div>
+//       <Label>
+//         <Input defaultChecked />
+//         <LabelText>Foo</LabelText>
+//       </Label>
+//       <Label>
+//         <Input />
+//         <LabelText $mode="dark">Foo</LabelText>
+//       </Label>
+//       <Label>
+//         <Input defaultChecked />
+//         <LabelText>Foo</LabelText>
+//       </Label>
+//       <Label>
+//         <Input defaultChecked />
+//         <LabelText $mode="dark">Foo</LabelText>
+//       </Label>
 //   </React.Fragment>
 //   )
+// }
+//
+// const Thing = styled.label`
 
+//   color: red;
+//   font-size:3em;
+
+// `
+// function App(){
+//       return(
+//         <Thing>
+//            hello world
+//         </Thing>
+  
+//       )
 // }
 
-const Input = styled.input.attrs({ type: "checkbox" })``;
 
-const Label = styled.label`
-  align-items: center;
-  display: flex;
-  gap: 8px;
-  margin-bottom: 8px;
-`
+// const Input = styled.input.attrs(props => ({
+//   // we can define static props
+//   type: "text",
 
-const LabelText = styled.span`
-  ${(props) => {
-    switch (props.$mode) {
-      case "dark":
-        return `
-          background-color: black;
-          color: white;
-          ${Input}:checked + && {
-            color: green;
-          }
-        `;
-      default:
-        return `
-          background-color: yellow;
-          color: black;
-          ${Input}:checked + && {
-            color: red;
-          }
-        `;
-    }
-  }}
+//   // or we can define dynamic ones
+//   size: props.size || "1em",
+// }))`
+//   color: palevioletred;
+//   font-size: 1em;
+//   border: 2px solid palevioletred;
+//   border-radius: 3px;
+
+//   /* here we use the dynamically computed prop */
+//   margin: ${props => props.size};
+//   padding: ${props => props.size};
+// `;
+
+
+// function App(){
+//         return(
+//           <div>
+//           <Input placeholder="A small text input" />
+//           <br />
+//           <Input placeholder="A bigger text input" size="2em" />
+//         </div>
+//         )
+//   }
+
+
+//rotate
+// const rotate = keyframes`
+//   from{
+//     transform :rotate(0deg);
+//   }
+//   to{
+//     transform: rotate(360deg);
+//   }
+// `;
+
+// const Rotate = styled.div`
+//   display: inline-block;
+//   animation: ${rotate} 2s linear infinite;
+//   margin: 3em;
+//   padding: 3em;
+  
+// `;
+
+
+// function App(){
+//   return(
+//     <Rotate>hello</Rotate>
+//   )
+// };
+
+//rotate
+
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
+`;
+
+const Rotate = styled.div`
+  animation: ${rotate} 1s linear infinite;
+  display: inline-block;
+  margin:2em;
+  padding:2em;
+
 `;
 function App(){
   return(
-    <React.Fragment>
-    <Label>
-      <Input defaultChecked />
-      <LabelText>one</LabelText>
-    </Label>
-
-    <Label>
-      <Input />
-      <LabelText $mode="dark">two</LabelText>
-    </Label>
-
-    <Label>
-      <Input defaultChecked />
-      <LabelText>three</LabelText>
-    </Label>
-
-    <Label>
-      <Input defaultChecked />
-      <LabelText $mode="dark">four</LabelText>
-    </Label>
-  </React.Fragment>
+    <Rotate> hello </Rotate>
   )
-}
-
-
-
+};
 
 export default App;
